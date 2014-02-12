c6cdde530681fbcac6f6448f487af617a90f3d77
Prathamesh <prathamesh.t@gmail.com>
2014-02-12 14:04:39 +0530

    Links Redefined

--- Links.thy
+++ Links.thy
@@ -215,10 +215,21 @@ and compabove. compbelow and compabove are further written as disjunction of man
 Compbelow refers to when the bottom row is extended or compressed. Compabove refers to when the 
 row above is extended or compressed*}
 
+definition linkrel_compress_top::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_compress_top x y \<equiv>  \<exists>B.((x = (basic (make_vert_block (nat (domain_block B - 1))))\<circ>(basic B))
+                              \<and>(y = (basic B)))"
+
+
+definition linkrel_compress_bottom::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_compress_bottom x y \<equiv>  \<exists>B.((x = (basic B)\<circ>(basic (make_vert_block (nat (domain_block B - 1)))))
+                              \<and>(y = (basic B)))"
+
+(*linkrel_compress*)
 definition linkrel_compress::"walls \<Rightarrow> walls \<Rightarrow> bool"
 where
-"linkrel_compress x y \<equiv>  \<exists>B.((x = (basic (make_vert_block (nat (domain_block B - 1))))\<circ>(basic B))
-                              \<and>(y = (basic B)))"
+"linkrel_compress x y = ((linkrel_compress_top x y) \<or> (linkrel_compress_bottom x y))"
 
 text{*slide relation refer to the relation where a crossing is slided over a vertical strand*}
 definition linkrel_slide::"walls \<Rightarrow> walls \<Rightarrow> bool"
@@ -233,19 +244,167 @@ where
 "linkrel x y = ((linkrel_uncross x y) \<or> (linkrel_pull x y) \<or> (linkrel_straighten x y) 
 \<or>(linkrel_swing x y)\<or>(linkrel_rotate x y) \<or> (linkrel_compress x y) \<or> (linkrel_slide x y)
 \<or>  (linkrel_uncross y x) \<or> (linkrel_pull y x) \<or> (linkrel_straighten y x) 
-\<or>(linkrel_swing y x)\<or>(linkrel_rotate y x) \<or> (linkrel_compress y x) \<or> (linkrel_slide y x))
-"
+\<or>(linkrel_swing y x)\<or>(linkrel_rotate y x) \<or> (linkrel_compress y x) \<or> (linkrel_slide y x))"
+
+(*linkrel fitting in diagrams*)
+definition linkrel_diagram_left::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_left x y \<equiv> \<exists>A.\<exists>B.\<exists>C.((x =  (A \<otimes> B))\<and> (y = (C \<otimes> B)) 
+                                     \<and> (linkrel A C))"
+
+definition linkrel_diagram_right::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_right x y \<equiv> \<exists>A.\<exists>B.\<exists>C.((x = (A \<otimes> B))\<and> (y = (A \<otimes> C)) 
+                                  \<and> (linkrel B C))"
+
+
+definition linkrel_diagram_center::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_center x y \<equiv> \<exists>A.\<exists>B1.\<exists>B2.\<exists>C.((x = (A \<circ> (B1::walls) \<otimes> C))
+                                           \<and> (y = (A \<circ> (B2::walls) \<otimes> C)) 
+                                     \<and> (linkrel B1 B2))"
+
+
+definition linkrel_diagram_middle_center::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_middle_center x y \<equiv> \<exists>A.\<exists>B.\<exists>C1.\<exists>C2.\<exists>D.\<exists>E.((x = (A \<circ> (B::walls) \<otimes> C1 \<otimes> D \<circ> E))
+                                           \<and> (y = (A \<circ> (B::walls) \<otimes> C2 \<otimes> D \<circ> E)) 
+                                     \<and> (linkrel C1 C2))"
+
+definition linkrel_diagram_middle_left:: "walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_middle_left x y \<equiv> \<exists>A.\<exists>B1.\<exists>B2.\<exists>C.\<exists>D.((x = (A \<circ> (B1::walls)\<otimes>C \<circ>D))
+                                           \<and> (y = (A \<circ> (B2::walls) \<otimes> C \<circ> D)) 
+                                     \<and> (linkrel B1 B2))"
+
+definition linkrel_diagram_middle_right::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_middle_right x y \<equiv> \<exists>A.\<exists>B.\<exists>C1.\<exists>C2.\<exists>D.((x = (A \<circ> (B::walls)\<otimes>C1\<circ> D))
+                                           \<and> (y =  (A \<circ> (B::walls) \<otimes> C2 \<circ> D)) 
+                                     \<and> (linkrel C1 C2))"
+
+definition linkrel_diagram_bottom_left::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_bottom_left x y \<equiv> \<exists>A1.\<exists>A2.\<exists>B.\<exists>C.((x = (((A1::walls) \<otimes> B) \<circ> C)) 
+                                                \<and>((y = (((A2::walls) \<otimes> B) \<circ> C))) 
+                                                \<and>(linkrel A1 A2))"
+                
+definition linkrel_diagram_bottom_right::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_bottom_right x y \<equiv> \<exists>A.\<exists>B1.\<exists>B2.\<exists>C.((x = (((A::walls) \<otimes> B1) \<circ> C)) 
+                                                \<and>((y = (((A::walls) \<otimes> B2) \<circ> C))) 
+                                                \<and>(linkrel B1 B2))"
+definition linkrel_diagram_bottom_center::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_bottom_center x y \<equiv> \<exists>A.\<exists>B1.\<exists>B2.\<exists>C.\<exists>D.((x = (((A::walls) \<otimes> B1 \<otimes> C) \<circ> D)) 
+                                                \<and>((y = (((A::walls) \<otimes> B2 \<otimes> C) \<circ> D))) 
+                                                \<and>(linkrel B1 B2))"
+
+definition linkrel_diagram_top_left::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_top_left x y \<equiv> \<exists>A.\<exists>B1.\<exists>B2.\<exists>C.((x = (A \<circ> ((B1::walls) \<otimes>  C)))
+                                                \<and>(y = (A \<circ> ((B2::walls) \<otimes> C)))
+                                                \<and>(linkrel B1 B2))"
+                
+definition linkrel_diagram_top_right::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_top_right x y \<equiv> \<exists>A.\<exists>B.\<exists>C1.\<exists>C2.((x = (A \<circ>((B::walls) \<otimes> C1))) 
+                                                 \<and>(y = (A \<circ> (B \<otimes> C2))) 
+                                                 \<and>(linkrel C1 C2))"
+definition linkrel_diagram_top_center::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_top_center x y \<equiv> \<exists>A.\<exists>B.\<exists>C1.\<exists>C2.\<exists>D.((x = (A \<circ>  ((B::walls) \<otimes> C1 \<otimes> D)))
+                                                \<and>(y = ((A \<circ> (B \<otimes> C2 \<otimes> D)))
+                                                \<and>(linkrel C1 C2)))"
+
+
+
+definition linkrel_diagram::"walls \<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram x y \<equiv> (
+(linkrel_diagram_left x y)\<or>(linkrel_diagram_right x y) \<or> (linkrel_diagram_center x y)
+\<or>(linkrel_diagram_middle_left x y)\<or>(linkrel_diagram_middle_right x y)
+\<or>(linkrel_diagram_middle_center x y)
+\<or>(linkrel_diagram_bottom_left x y)\<or>(linkrel_diagram_bottom_left x y)
+\<or>(linkrel_diagram_bottom_center x y)
+\<or>(linkrel_diagram_top_left x y)\<or>(linkrel_diagram_top_right x y)\<or>(linkrel_diagram_top_center x y)
+)"
+
+(*proving the symmetry of linkrel_diagram*)
+lemma symm_left:"(linkrel_diagram_left x y) \<Longrightarrow> (linkrel_diagram_left y x)"
+ using linkrel_def linkrel_diagram_left_def by auto
+
+lemma symm_right: "(linkrel_diagram_right x y) \<Longrightarrow> (linkrel_diagram_right y x)"
+ using linkrel_def linkrel_diagram_right_def by auto
+
+lemma symm_center:"(linkrel_diagram_center x y) \<Longrightarrow> (linkrel_diagram_center y x)"
+ using linkrel_def linkrel_diagram_center_def by auto
+
+lemma symm_middle_right:"(linkrel_diagram_middle_right x y) \<Longrightarrow> (linkrel_diagram_middle_right y x)"
+ unfolding linkrel_def linkrel_diagram_middle_right_def by metis
+
+lemma symm_middle_left:"(linkrel_diagram_middle_left x y) \<Longrightarrow> (linkrel_diagram_middle_left y x)"
+ unfolding linkrel_def linkrel_diagram_middle_left_def by metis
+
+lemma symm_middle_center:"(linkrel_diagram_middle_center x y) \<Longrightarrow> (linkrel_diagram_middle_center y x)"
+ unfolding linkrel_def linkrel_diagram_middle_center_def by metis
+
+lemma symm_bottom_left: "(linkrel_diagram_bottom_left x y) \<Longrightarrow> (linkrel_diagram_bottom_left y x)"
+ unfolding linkrel_def linkrel_diagram_bottom_left_def by metis
+
+lemma symm_bottom_right: "(linkrel_diagram_bottom_right x y) \<Longrightarrow> (linkrel_diagram_bottom_right y x)"
+ unfolding linkrel_def linkrel_diagram_bottom_right_def by metis
+
+lemma symm_bottom_center:"(linkrel_diagram_bottom_center x y) \<Longrightarrow> (linkrel_diagram_bottom_center y x)"
+ unfolding linkrel_def linkrel_diagram_bottom_center_def by metis
+
+lemma symm_top_left:"(linkrel_diagram_top_left x y) \<Longrightarrow> (linkrel_diagram_top_left y x)"
+ unfolding linkrel_def linkrel_diagram_top_left_def by metis
+
+lemma symm_top_right: "(linkrel_diagram_top_right x y) \<Longrightarrow> (linkrel_diagram_top_right y x)"
+ unfolding linkrel_def linkrel_diagram_top_right_def by metis
+
+lemma symm_top_center:"(linkrel_diagram_top_center x y) \<Longrightarrow> (linkrel_diagram_top_center y x)"
+ unfolding linkrel_def linkrel_diagram_top_center_def by metis
+
+lemma symm_linkrel_diagram:"(linkrel_diagram x y)\<Longrightarrow> (linkrel_diagram y x)"
+unfolding linkrel_diagram_def using symm_bottom_center symm_bottom_left symm_center 
+symm_left symm_middle_center symm_middle_left symm_middle_right symm_right symm_top_center 
+symm_top_left symm_top_right
+ by metis
+
+(*linkrel_diagram fitting in links*)
+
+definition linkrel_diagram_equiv::"walls\<Rightarrow> walls \<Rightarrow> bool"
+where
+"linkrel_diagram_equiv = (linkrel_diagram)^**"
+
+lemma symm_linkrel_diagram_equiv:"linkrel_diagram_equiv x y \<Longrightarrow> (linkrel_diagram_equiv y x)"
+      using linkrel_diagram_equiv_def symm_linkrel_diagram symmetry3 symp_def
+      by metis
+
+definition link_equiv::"diagram\<Rightarrow> diagram \<Rightarrow> bool"
+where
+"link_equiv x y = (linkrel_diagram_equiv (Rep_diagram x) (Rep_diagram y))"
+
+lemma "(link_equiv x x)"
+unfolding link_equiv_def linkrel_diagram_equiv_def by auto
+
+lemma "(link_equiv x y) \<Longrightarrow> (link_equiv y x)"
+unfolding link_equiv_def using symm_linkrel_diagram_equiv by auto
+
+lemma "(link_equiv x y)\<and> (link_equiv y z) \<Longrightarrow> (link_equiv x z)"
+using link_equiv_def linkrel_diagram_equiv_def rtranclp_trans by (metis)
 
 text{*we defined the link relations for a framed link, which are the same as those of unframed 
 links excluding the uncross relation which straightens out a cross*}
-
+(* we need to rewrite the definitions of framed_link_relations and repeat the above for framed_link_relations
 definition framed_linkrel::"walls =>walls\<Rightarrow>bool"
 where
 "framed_linkrel x y = ((framed_linkrel_uncross x y) \<or> (linkrel_pull x y) \<or> (linkrel_straighten x y) 
 \<or>(linkrel_swing x y)\<or>(linkrel_rotate x y) \<or> (linkrel_compress x y) \<or> (linkrel_slide x y)
 \<or>  (framed_linkrel_uncross y x) \<or> (linkrel_pull y x) \<or> (linkrel_straighten y x) 
-\<or>(linkrel_swing y x)\<or>(linkrel_rotate y x) \<or> (linkrel_compress y x) \<or> (linkrel_slide y x))
-"
+\<or>(linkrel_swing y x)\<or>(linkrel_rotate y x) \<or> (linkrel_compress y x) \<or> (linkrel_slide y x))"
 
 text{*Following lemmas asserts that if two framed linked diagrams are equivalent, then the unframed 
 links are equivalent*}
@@ -313,18 +472,19 @@ by (metis (full_types) framed_linkrel_equiv_def)
 lemma linkrel_trans: assumes "linkrel_equiv x y" and "linkrel_equiv y z"
 shows "linkrel_equiv x z"
 using rtranclp_trans linkrel_equiv_def  by (metis (full_types) assms(1) assms(2))
-
+*)
 text{*links upto equivalence are well defined*}
 text{*Link- Definition and the proof of being well defined*}
-(*
-quotient_type Link = "diagram" / "linkrel_equiv"
+
+quotient_type Link = "diagram" / "link_equiv"
  morphisms Rep_links Abs_links
 proof (rule equivpI)
-show "reflp linkrel_equiv" unfolding reflp_def reflective by (metis reflective)
-show "symp linkrel_equiv" using link_symmetry by auto
-show "transp linkrel_equiv" unfolding transp_def linkrel_equiv_def rtranclp_trans by auto  
+show "reflp link_equiv" unfolding reflp_def using link_equiv_def linkrel_diagram_equiv_def rtranclp.rtrancl_refl
+  by (metis (full_types) )
+show "symp link_equiv" by (metis link_equiv_def symm_linkrel_diagram_equiv sympI)
+show "transp link_equiv" unfolding transp_def link_equiv_def rtranclp_trans by (metis linkrel_diagram_equiv_def rtranclp_trans) 
 qed
-
+(*
 quotient_type Framed_Link = "diagram" / "framed_linkrel_equiv"
  morphisms Rep_framed_links Abs_framed_links
 proof (rule equivpI)

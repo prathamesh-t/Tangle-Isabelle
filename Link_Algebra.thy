19b3581e78d1c842740a4819bbd48d71f90f1cf0
Prathamesh <prathamesh.t@gmail.com>
2014-02-28 11:27:50 +0530

    changes

--- Link_Algebra.thy
+++ Link_Algebra.thy
@@ -7,13 +7,13 @@ empty brick commutes with regards to the tensor product. This properties holds t
 empty_tensor*}
 
 locale empty_tensor=
- fixes rel::"walls \<Rightarrow> walls \<Rightarrow> bool" (infixl "~" 65)
+ fixes rel::"wall \<Rightarrow> wall \<Rightarrow> bool" (infixl "~" 65)
 assumes left: "(x \<otimes> (basic (cement empty))) ~ x"
     and right:"((basic (cement empty)) \<otimes> x) ~ x"
 context empty_tensor
- begin
+begin
 
-text{*The fact that this property holds true for tensor between walls implies that it holds true 
+text{*The fact that this property holds true for tensor between wall implies that it holds true 
 for blocks *}
 (*
 theorem left_concatenate:"(x::block) \<otimes> (cement empty) = x"
@@ -58,53 +58,60 @@ locale empty_compose = empty_tensor +
 assumes domain_compose: "(domain_wall x = 0) \<Longrightarrow>(basic (cement empty)) \<circ> x ~ x"
 and codomain_compose: "(codomain_wall x = 0) \<Longrightarrow>(x \<circ> (basic (cement empty))) ~  x"
 
-text{*If two walls are related by linkrel, then they give rise to the same tangles in this locale*}
+text{*If two wall are related by linkrel, then they give rise to the same tangles in this locale*}
 
 locale Equivalence = empty_tensor + empty_compose+
  assumes equality:"linkrel x y \<Longrightarrow> x ~ y"
- assumes "((B::walls) ~ D) \<and> ((A::walls) ~ C)
+ assumes "((B::wall) ~ D) \<and> ((A::wall) ~ C)
          \<and>(well_defined_tangle A)\<and>(well_defined_tangle B)
          \<and>(well_defined_tangle C)\<and>(well_defined_tangle D) 
          \<and>(domain_wall B)= (codomain_wall A)
          \<and>(domain_wall D)= (codomain_wall C)
-                \<Longrightarrow>((A::walls) \<circ> B) ~ (C \<circ> D)"
- assumes tensor_eq:"((B::walls) ~ D) \<and> ((A::walls) ~ C) \<and>(well_defined_tangle A)\<and>(well_defined_tangle B)
- \<and>(well_defined_tangle C)\<and>(well_defined_tangle D) \<Longrightarrow>((A::walls) \<otimes> B) ~ (C \<otimes> D)"
+                \<Longrightarrow>((A::wall) \<circ> B) ~ (C \<circ> D)"
+ assumes tensor_eq:"((B::wall) ~ D) \<and> ((A::wall) ~ C) \<and>(well_defined_tangle A)\<and>(well_defined_tangle B)
+ \<and>(well_defined_tangle C)\<and>(well_defined_tangle D) \<Longrightarrow>((A::wall) \<otimes> B) ~ (C \<otimes> D)"
  assumes refl:"A ~A"
  assumes sym:"A~B \<Longrightarrow> B~A"
  assumes trans:" (A~B) \<and> (B~C) \<Longrightarrow> (A~C)"
 
 locale Tangle_Equivalence = Equivalence +
- fixes tangle_equivalence::"Tangle \<Rightarrow> Tangle \<Rightarrow> bool" (infixl "~" 65) 
- assumes Tangle_equality:"(x ~ y) \<Longrightarrow> ((Abs_Tangle x) ~ (Abs_Tangle y))"
+ fixes tangle_equivalence::"Tangle_Diagram \<Rightarrow> Tangle_Diagram \<Rightarrow> bool" (infixl "~" 65) 
+ assumes Tangle_equality:"(x ~ y) \<Longrightarrow> ((Abs_Tangle_Diagram x) ~ (Abs_Tangle_Diagram y))"
  begin
 
 
 lemma assumes "linkrel A B"
-shows "Abs_Tangle A ~ Abs_Tangle B" using equality assms Tangle_equality by auto
+shows "(Abs_Tangle_Diagram A) ~ (Abs_Tangle_Diagram B)" using equality assms Tangle_equality by auto
 
 
 text{*definition of a link as a boolean function*}
-
-definition Link::"Tangle \<Rightarrow> bool"
+(*modify this*)
+definition Link::"Tangle_Diagram \<Rightarrow> bool"
 where
- "Link x \<equiv> (well_defined (Rep_Tangle x))"
+ "Link x \<equiv> ((abs (domain_wall (Rep_Tangle_Diagram x)) + abs (codomain_wall (Rep_Tangle_Diagram x))) = 0)"
 
-definition Equivalent_Links::"Tangle \<Rightarrow> Tangle  \<Rightarrow> bool"
+definition Equivalent_Links::"Tangle_Diagram \<Rightarrow> Tangle_Diagram  \<Rightarrow> bool"
 where
-"Equivalent_Links x y \<equiv> (Link x) \<and> (Link y) \<and> (x = y)"
+"Equivalent_Links x y \<equiv> (Link x) \<and> (Link y) \<and> (x ~ y)"
 
 
-lemma "basic (cement empty) = (basic (cement empty)) \<circ> (basic (cement empty))"
- by (metis domain.simps(6) domain_block.simps(1) domain_compose domain_wall.simps(1))
+lemma "basic (cement empty) ~ (basic (cement empty)) \<circ> (basic (cement empty))"
+by (metis (full_types) codomain.simps(6) codomain_block.simps(1) codomain_compose codomain_wall.simps(1) sym)
 end
 
 sublocale Tangle_Equivalence < Equivalence
  by unfold_locales 
 
+
 locale Tangle_Invariant = Equivalence +
-fixes funct::"walls \<Rightarrow> 'a" 
-assumes "(x = y) \<Longrightarrow> (funct x) = (funct y)"
+fixes funct::"wall \<Rightarrow> 'a" 
+assumes invariance:"(rel x y) \<Longrightarrow> (funct x) = (funct y)"
+begin
+
+lemma "((funct x) \<noteq> (funct y)) \<longrightarrow> \<not> (x ~ y)" 
+    using invariance by auto
+
+end
 
 end
 
--- Tangle_Algebra.thy
+++ Tangle_Algebra.thy
@@ -19,11 +19,11 @@ lemma codomain_make_vert:"codomain_block (make_vert_block n) = int n"
    apply(auto)
    done
   
-(*we defined the tensor of walls as described in the standard tangle algebra*)
-fun tensor::"walls => walls => walls" (infixr "\<otimes>" 65)
+(*we defined the tensor of wall as described in the standard tangle algebra*)
+fun tensor::"wall => wall => wall" (infixr "\<otimes>" 65)
 where
 1:"tensor (basic x) (basic y) = (basic (x \<otimes> y))"
-|2:"tensor (x*xs) (basic y) = (
+|evss:"tensor (x*xs) (basic y) = (
                   if (codomain_block y = 0)
                     then (x \<otimes> y)*xs 
                     else (x \<otimes> y)*(xs\<otimes>(basic (make_vert_block (nat (codomain_block y)))))
@@ -36,14 +36,16 @@ where
 |4:"tensor (x*xs) (y*ys) = (x \<otimes> y)* (xs \<otimes> ys)"
 
 (*extending the definition of tensors to diagrams*)
-definition tensor_Tangle ::"Tangle \<Rightarrow> Tangle \<Rightarrow> Tangle" (infixl "\<otimes>" 65)
+definition tensor_Tangle ::"Tangle_Diagram \<Rightarrow> Tangle_Diagram \<Rightarrow> Tangle_Diagram" (infixl "\<otimes>" 65)
 where
-"tensor_Tangle x y = Abs_Tangle ((Rep_Tangle x) \<otimes> (Rep_Tangle y))" 
+"tensor_Tangle x y = Abs_Tangle_Diagram ((Rep_Tangle_Diagram x) \<otimes> (Rep_Tangle_Diagram y))" 
 
 (*some lemmas*)
 lemma "tensor (basic (cement vert)) (basic (cement vert)) = (basic ((cement vert) \<otimes> (cement vert)))"
 by simp
 
+(*domain_wall of a tensor product of two walls is the sum of the domain_wall of each of the tensor
+ products*)
 lemma tensor_domain_wall_additivity:
  "domain_wall (xs \<otimes> ys) = domain_wall xs + domain_wall ys"
  proof(cases xs)
@@ -146,59 +148,269 @@ lemma tensor_domain_wall_additivity:
     then show ?thesis by auto
   qed
 
+(*the following theorem tells us that given a tensor product of a tangle diagram with a vertical
+   strand, it gives a tangle diagram*)
+theorem is_tangle_make_vert_right:
+ "(is_tangle_diagram xs) \<Longrightarrow> is_tangle_diagram (xs \<otimes> (basic (make_vert_block n)))"
+proof(induct xs)
+ case (basic xs)
+  show ?case using tensor_def by auto
+ next
+ case (prod x xs)
+  have ?case 
+   proof(cases n)
+    case 0
+     have 
+        "codomain_block (x \<otimes> (make_vert_block 0)) 
+               = (codomain_block x) + codomain_block(make_vert_block 0)"
+            using  codomain_additive by auto
+     moreover have "codomain_block (make_vert_block 0) = 0" 
+            by auto
+     ultimately have "codomain_block (x \<otimes> (make_vert_block 0)) = codomain_block (x)"
+            by auto
+     moreover have "is_tangle_diagram xs" 
+          using prod.prems by (metis is_tangle_diagram.simps(2))
+     then have "is_tangle_diagram ((x \<otimes> (make_vert_block 0))*xs)" 
+          using is_tangle_diagram.simps(2) by (metis calculation prod.prems)
+     then  have "is_tangle_diagram ((x*xs) \<otimes> (basic (make_vert_block 0)))" 
+          by auto
+     then show ?thesis using  "0" by (metis)
+    next
+    case (Suc k)
+     have "codomain_block (make_vert_block (k+1)) = int (k+1)" 
+         using codomain_make_vert  by auto
+     then have "(nat (codomain_block (make_vert_block (k+1)))) = k+1" 
+         by auto
+     then have "make_vert_block (nat (codomain_block (make_vert_block (k+1)))) 
+                             = make_vert_block (k+1)"
+          by auto
+     moreover have "codomain_wall (basic (make_vert_block (k+1)))>0" 
+       using make_vert_block.simps codomain_wall.simps  Suc_eq_plus1 
+       codomain_make_vert of_nat_0_less_iff zero_less_Suc
+       by metis
+     ultimately have 1: "(x*xs) \<otimes> (basic (make_vert_block (k+1))) 
+             =  (x\<otimes>(make_vert_block (k+1)))*(xs\<otimes>(basic (make_vert_block (k+1))))" 
+        using tensor.simps(2)  by (metis codomain_wall.simps(1) int_0 of_nat_less_0_iff)
+     have "domain_wall (xs\<otimes>(basic (make_vert_block (k+1)))) 
+                 = domain_wall xs + domain_wall (basic (make_vert_block (k+1)))"
+            using tensor_domain_wall_additivity by auto
+     then have 2: 
+          "domain_wall (xs\<otimes>(basic (make_vert_block (k+1)))) 
+                  = (domain_wall xs) + int (k+1)"
+             using domain_make_vert domain_wall.simps(1) by auto 
+     moreover have 3: "codomain_block (x \<otimes> (make_vert_block (k+1))) 
+                 = codomain_block x + int (k+1)"
+             using  codomain_additive codomain_make_vert by (metis)
+     have "is_tangle_diagram (x*xs)"
+            using prod.prems by auto
+     then have 4:"codomain_block x = domain_wall xs"
+            using is_tangle_diagram.simps(2) by metis
+     from 2 3 4 have 
+       "domain_wall (xs\<otimes>(basic (make_vert_block (k+1)))) 
+               = codomain_block (x \<otimes> (make_vert_block (k+1))) "
+            by auto
+     moreover have "is_tangle_diagram (xs\<otimes>(basic (make_vert_block (k+1))))"
+            using prod.hyps prod.prems by (metis Suc Suc_eq_plus1 is_tangle_diagram.simps(2))
+     ultimately have "is_tangle_diagram ((x*xs) \<otimes> (basic (make_vert_block (k+1))))"
+                using 1 by auto
+      then show ?thesis using  Suc Suc_eq_plus1 by metis
+    qed
+   then show ?case by auto
+ qed
+
+(*the following theorem tells us that given a tensor product of a vertical strand block with a 
+tangle diagram, it gives a tangle diagram. If the above result can be treated as right product, 
+this can be treated as the left product*)
+
+theorem is_tangle_make_vert_left:
+ "(is_tangle_diagram xs) \<Longrightarrow> is_tangle_diagram ((basic (make_vert_block n)) \<otimes> xs)"
+proof(induct xs)
+ case (basic xs)
+  show ?case using tensor_def by auto
+ next
+ case (prod x xs)
+  have ?case 
+   proof(cases n)
+    case 0
+     have 
+        "codomain_block ( (make_vert_block 0) \<otimes> x) 
+               = (codomain_block x) + codomain_block(make_vert_block 0)"
+            using  codomain_additive by auto
+     moreover have "codomain_block (make_vert_block 0) = 0" 
+            by auto
+     ultimately have "codomain_block ( (make_vert_block 0) \<otimes> x) = codomain_block (x)"
+            by auto
+     moreover have "is_tangle_diagram xs" 
+          using prod.prems by (metis is_tangle_diagram.simps(2))
+     then have "is_tangle_diagram (( (make_vert_block 0) \<otimes> x)*xs)" 
+          using is_tangle_diagram.simps(2) by (metis calculation prod.prems)
+     then  have "is_tangle_diagram ((basic (make_vert_block 0)) \<otimes> (x*xs) )" 
+          by auto
+     then show ?thesis using  "0" by (metis)
+    next
+    case (Suc k)
+     have "codomain_block (make_vert_block (k+1)) = int (k+1)" 
+         using codomain_make_vert  by auto
+     then have "(nat (codomain_block (make_vert_block (k+1)))) = k+1" 
+         by auto
+     then have "make_vert_block (nat (codomain_block (make_vert_block (k+1)))) 
+                             = make_vert_block (k+1)"
+          by auto
+     moreover have "codomain_wall (basic (make_vert_block (k+1)))>0" 
+       using make_vert_block.simps codomain_wall.simps  Suc_eq_plus1 
+       codomain_make_vert of_nat_0_less_iff zero_less_Suc
+       by metis
+     ultimately have 1: "  (basic (make_vert_block (k+1))) \<otimes> (x*xs)
+             =  ((make_vert_block (k+1)) \<otimes> x)*((basic (make_vert_block (k+1))) \<otimes> xs)" 
+        using tensor.simps(3)  by (metis codomain_wall.simps(1) int_0 of_nat_less_0_iff)
+     have "domain_wall ((basic (make_vert_block (k+1))) \<otimes> xs) 
+                 = domain_wall xs + domain_wall (basic (make_vert_block (k+1)))"
+            using tensor_domain_wall_additivity by auto
+     then have 2: 
+          "domain_wall ((basic (make_vert_block (k+1))) \<otimes> xs) 
+                  = (domain_wall xs) + int (k+1)"
+             using domain_make_vert domain_wall.simps(1) by auto 
+     moreover have 3: "codomain_block ( (make_vert_block (k+1)) \<otimes> x) 
+                 = codomain_block x + int (k+1)"
+             using  codomain_additive codomain_make_vert
+              comm_semiring_1_class.normalizing_semiring_rules(24) 
+             by metis
+     have "is_tangle_diagram (x*xs)"
+            using prod.prems by auto
+     then have 4:"codomain_block x = domain_wall xs"
+            using is_tangle_diagram.simps(2) by metis
+     from 2 3 4 have 
+       "domain_wall ((basic (make_vert_block (k+1))) \<otimes> xs) 
+               = codomain_block ((make_vert_block (k+1)) \<otimes> x)"
+             by auto
+     moreover have "is_tangle_diagram ((basic (make_vert_block (k+1))) \<otimes> xs)"
+            using prod.hyps prod.prems by (metis Suc Suc_eq_plus1 is_tangle_diagram.simps(2))
+     ultimately have "is_tangle_diagram ((basic (make_vert_block (k+1))) \<otimes> (x*xs))"
+                using 1 by auto
+      then show ?thesis using  Suc Suc_eq_plus1 by metis
+    qed
+   then show ?case by auto
+ qed
+
+(*tensor product of two tangle diagrams is a tangle diagram*)
+theorem is_tangle_diagramness:
+ shows"(is_tangle_diagram x)\<and>(is_tangle_diagram y)\<longrightarrow>is_tangle_diagram (tensor x y)"
+ proof(induct_tac x y  rule:tensor.induct)
+ fix z w
+ let ?case = "(is_tangle_diagram (basic z))\<and>(is_tangle_diagram (basic w))
+                  \<longrightarrow>is_tangle_diagram ((basic z) \<otimes> (basic w))"
+  show ?case by auto
+ next
+ fix z zs w 
+   assume a: "codomain_block w \<noteq> 0 "
+   assume b: "(is_tangle_diagram zs)
+               \<and>(is_tangle_diagram (basic (make_vert_block (nat (codomain_block w))))) 
+    \<longrightarrow> is_tangle_diagram (zs \<otimes> basic (make_vert_block (nat (codomain_block w))))"
+   let ?case = 
+ "is_tangle_diagram (z * zs) \<and> is_tangle_diagram (basic w) \<longrightarrow> is_tangle_diagram (z * zs \<otimes> basic w)"
+  have "is_tangle_diagram (z * zs) \<and> is_tangle_diagram (basic w) \<longrightarrow>
+               is_tangle_diagram ((z*zs) \<otimes> (basic w))"
+  proof-
+  have " is_tangle_diagram (z * zs) \<and> is_tangle_diagram (basic w) \<longrightarrow> is_tangle_diagram zs"
+        by (metis is_tangle_diagram.simps(2))
+  moreover have "(is_tangle_diagram (basic (make_vert_block (nat (codomain_block w))))) "
+              using is_tangle_diagram.simps(1) by auto
+  ultimately have "is_tangle_diagram (z * zs) \<and> is_tangle_diagram (basic w) \<longrightarrow>
+                is_tangle_diagram (zs \<otimes> basic (make_vert_block (nat (codomain_block w))))"
+         using b by auto
+  moreover have 1:"codomain_block w = domain_wall (basic (make_vert_block (nat (codomain_block w))))"
+        using codomain_block_nonnegative domain_make_vert domain_wall.simps(1) int_nat_eq by auto
+  moreover have 2:"is_tangle_diagram (z * zs) \<and> is_tangle_diagram (basic w) \<longrightarrow>
+                 codomain_block z = domain_wall zs"
+       using is_tangle_diagram.simps(2) by metis
+ moreover have "codomain_block (z \<otimes> w) = codomain_block z +codomain_block w"
+       using codomain_additive by auto
+ moreover have "domain_wall (zs \<otimes> (basic (make_vert_block (nat (codomain_block w)))))
+                 = domain_wall zs + domain_wall (basic (make_vert_block (nat (codomain_block w))))"
+          using tensor_domain_wall_additivity by auto
+ moreover then have " is_tangle_diagram (z * zs) \<and> is_tangle_diagram (basic w) \<longrightarrow>
+          domain_wall (zs \<otimes> (basic (make_vert_block (nat (codomain_block w)))))
+                   = codomain_block (z \<otimes> w)"
+             by (metis "1" "2" calculation(4))
+ ultimately have 
+        "is_tangle_diagram (z * zs) \<and> is_tangle_diagram (basic w) \<longrightarrow>
+         is_tangle_diagram ((z \<otimes> w)* (zs \<otimes> (basic (make_vert_block (nat (codomain_block w))))))"
+          using is_tangle_diagram.simps(2)  by auto
+ then have "is_tangle_diagram (z * zs) \<and> is_tangle_diagram (basic w) \<longrightarrow>
+               is_tangle_diagram ((z*zs) \<otimes> (basic w))"
+               using a by auto
+ then show ?thesis by auto
+ qed
+ then have ?case by auto
+ then have " (codomain_block w \<noteq> 0 \<Longrightarrow>
+        is_tangle_diagram zs \<and> is_tangle_diagram (basic (make_vert_block (nat (codomain_block w)))) \<longrightarrow>
+        is_tangle_diagram (zs \<otimes> basic (make_vert_block (nat (codomain_block w))))) \<Longrightarrow>
+       is_tangle_diagram (z * zs) \<and> is_tangle_diagram (basic w) \<longrightarrow> is_tangle_diagram (z * zs \<otimes> basic w)"
+        by auto
+ 
+  
 
 (*
-theorem well_definedness:
- assumes "well_defined_tangle x" 
-    and "well_defined_tangle y" 
- shows"well_defined_tangle (tensor x y)"
-proof(induct_tac x y rule: tensor.induct)
- fix x y                 
- let ?case = " well_defined_tangle (basic x \<otimes> basic y)"
-    have "domain_codomain_list (basic x \<otimes> basic y) = []" 
-        by auto
-    from this have "list_sum (domain_codomain_list (basic x \<otimes> basic y)) = 0" 
-       by auto   
-    from this show ?case unfolding well_defined_tangle_def by auto
- next
- fix x y xs
- assume A:"codomain_block y \<noteq> 0"
- assume B:"well_defined_tangle (xs \<otimes> basic (make_vert_block (nat (codomain_block y - 1))))"
- assume C:"well_defined_tangle (x*xs)" 
- let ?case = "well_defined_tangle (x * xs \<otimes> basic y)"
-   have 1:"(x*xs) \<otimes> (basic y) =  (x \<otimes> y)*(xs\<otimes>(basic (make_vert_block (nat (codomain_block y- 1)))))"
+proof(cases x)
+case (basic z)
+ have "(is_tangle_diagram (basic z))\<and>(is_tangle_diagram y)\<Longrightarrow>is_tangle_diagram ((basic z) \<otimes> y)"
+  proof(cases y)
+  case (basic w)
+    show ?thesis using basic by auto
+  next
+  case (prod w ws)
+    have "(is_tangle_diagram (basic z))\<and>(is_tangle_diagram (w*ws))
+                \<Longrightarrow>is_tangle_diagram ((basic z)\<otimes>(w*ws))"
+        proof-
+        assume A: "(is_tangle_diagram (basic z))\<and>(is_tangle_diagram (w*ws))"
+        have 1:"codomain_block w = domain_wall ws"
+                  using A by (metis is_tangle_diagram.simps(2))
+        have "(codomain_block z) = 0 \<Longrightarrow> (basic z)\<otimes>(w*ws) = (z \<otimes> w)*ws"
+             using tensor.simps by auto
+        moreover have "(codomain_block z) = 0 \<Longrightarrow> codomain_block (z \<otimes> w) = codomain_block w"
+                      using codomain_additive by auto
+        moreover have 2:"domain_wall ws = codomain_block w"
+                       using A is_tangle_diagram.simps(2) by metis
+        moreover have 3: "is_tangle_diagram ws" 
+                      using  A is_tangle_diagram.simps(2) by metis
+        ultimately have 4: "(codomain_block z) = 0 \<Longrightarrow> is_tangle_diagram ((basic z)\<otimes>(w*ws))"
+                     by auto
+*)        
+ 
+        
+      
+
+(*
+  fix x y                 
+  let ?case = "is_tangle_diagram (basic x \<otimes> basic y)"
+    show ?case by auto 
+ next 
+  fix x y xs
+  assume A:"codomain_block y \<noteq> 0"
+  assume B:"is_tangle_diagram (xs \<otimes>  basic (make_vert_block (nat (codomain_block y))))"
+  assume C: "(is_tangle_diagram (x*xs))\<and>(is_tangle_diagram (basic y)) "
+  let ?case = "is_tangle_diagram ((x * xs) \<otimes> basic y)"
+   have 1:"(x*xs) \<otimes> (basic y) =  (x \<otimes> y)*(xs\<otimes>(basic (make_vert_block (nat (codomain_block y)))))"
     using tensor.simps(2) A by auto
-  moreover have "list_sum (domain_codomain_list (x*xs)) = 0" 
-    using well_defined_tangle_def C by auto
-  moreover then have "codomain_block x = domain_wall xs" 
-    using well_defined_tangle_def C domain_codomain_list_def
-              abs_zero_equality calculation(2) codomain_wall.simps(1) compose_Nil 
-              diagram_wall_count_list_zero  
-    by metis
- moreover have "domain_wall (xs \<otimes> (basic (make_vert_block (nat (codomain_block y- 1)))))
-                  = domain_wall xs + domain_block  (make_vert_block (nat (codomain_block y- 1)))"
-           using tensor.simps  domain_wall.simps(1) tensor_domain_wall_additivity by auto
- moreover have " domain_block  (make_vert_block (nat (codomain_block y- 1))) = 
-                             codomain_block y" 
-      using domain_make_vert int_def 
-      by (metis A add_eq_if codomain_block_nonnegative 
-          comm_semiring_1_class.normalizing_semiring_rules(24) 
-         int_nat_eq nat_diff_distrib' of_nat_0 of_nat_Suc transfer_nat_int_numerals(2) zero_le_one)
- ultimately have "domain_wall (xs \<otimes> (basic (make_vert_block (nat (codomain_block y- 1)))))
-                      = codomain_block x + codomain_block y"
-               by auto
- then have 2:"domain_wall (xs \<otimes> (basic (make_vert_block (nat (codomain_block y- 1))))) 
-                      = codomain_block (x \<otimes> y)"
-       using  codomain_additive by (metis)
- moreover have "list_sum (domain_codomain_list (xs \<otimes> (basic (make_vert_block (nat (codomain_block y- 1)))))) 
-              = 0"
-            using well_defined_tangle_def B by auto
- moreover have "list_sum (domain_codomain_list ((x*xs) \<otimes> (basic y))) = 
-list_sum (domain_codomain_list (xs \<otimes> (basic (make_vert_block (nat (codomain_block y- 1)))))) 
-+ abs( domain_wall (xs \<otimes> (basic (make_vert_block (nat (codomain_block y- 1))))) - codomain_wall(basic (x \<otimes> y)))"
-  using list_sum_def 2 
-by (metis "1" codomain_wall.simps(1) comm_semiring_1_class.normalizing_semiring_rules(24) domain_codomain_list.simps(2) list_sum.simps(2))
- ultimately have "list_sum (domain_codomain_list ((x*xs) \<otimes> (basic y))) = 0"
-           by (metis codomain_wall.simps(1) comm_monoid_add_class.add.left_neutral diff_self wall_count_list_list_sum_abs)
-*)
-end
+   have "codomain_block (x \<otimes> y) = codomain_block x + codomain_block y"
+        using codomain_additive by auto
+   moreover have "domain_wall (xs\<otimes>(basic (make_vert_block (nat (codomain_block y)))))
+                = domain_wall xs + domain_wall (basic (make_vert_block (nat (codomain_block y))))"
+         using tensor_domain_wall_additivity by auto
+  moreover have "domain_wall (basic (make_vert_block (nat (codomain_block y))))
+                         = codomain_block y"
+             using codomain_block_nonnegative domain_make_vert domain_wall.simps(1) int_nat_eq 
+             by auto
+   moreover have "is_tangle_diagram (x*xs) \<Longrightarrow> domain_wall xs = codomain_block x"
+           using  is_tangle_diagram.simps(2) by metis
+   ultimately have "is_tangle_diagram (x*xs) 
+                        \<Longrightarrow> (domain_wall (xs\<otimes>(basic (make_vert_block (nat (codomain_block y)))))
+                               = codomain_block (x \<otimes> y))"   
+                   by auto
+   then have "is_tangle_diagram (x*xs) 
+                        \<Longrightarrow> is_tangle_diagram ((x*xs) \<otimes> (basic y))" 
+                  by (metis "1" is_tangle_diagram.simps(2) is_tangle_make_vert_right)
+   then have "is_tangle_diagram ((x*xs) \<otimes> (basic y))" 
+               using C by auto
+   
+*)        

8acc5b11f25f3981bbc0af5ac6a1f84b2fd4653d
Prathamesh <prathamesh.t@gmail.com>
2014-04-15 14:11:58 +0530

    Recursive_Component_Tested

--- Rec_Comp.thy
+++ Rec_Comp.thy
@@ -3,92 +3,86 @@ imports Link_Algebra
 begin
 
 
-(*we begin by defining two different types of an end of a strand, domain refers to the fact the 
+(*we begin by defining two different types of an end of a strand, domain refers to the fact the
 other end of a strand lies in the domain of the wall and codomain refers to the fact that other end
-of a strand lies in the codomain*) 
-datatype endtype = domain|codomain
+of a strand lies in the codomain*)
+datatype endtype = domain | codomain
+
 
 (*endpoint of a strand is given by the type of the end point and a natural number which refers
-to the  position of the other end of the strand*)  
-type_synonym endpt = "endtype \<times> nat"
+to the  position of the other end of the strand*) 
 
-(*The following functions are used to abbreviate (domain, n) and (codomain, n) as (dom n) and 
-(codom n). The reason behind introducing these functions is purely for the sake of 
-comprehensibility of the code*)
-
-definition dom::"nat \<Rightarrow> endpt"
-where
-"dom n \<equiv> (domain, n)"
-
-definition codom::"nat \<Rightarrow> endpt"
-where
-"codom n \<equiv> (codomain, n)"
+datatype endpt = dom nat|codom nat
 
 (*the function type tells us the endtype of an endpt*)
 definition type::"endpt \<Rightarrow> endtype"
 where
-"type x = (fst x)"
+"type x = (case x of dom n \<Rightarrow> domain|codom n \<Rightarrow> codomain)"
+
 (*the function str_number tells us the strand number of an endpt*)
 definition str_number::"endpt \<Rightarrow> nat"
 where
-"str_number x = (snd x)"
+"str_number x = (case x of dom n \<Rightarrow> n|codom n \<Rightarrow> n)"
 
+(*the following function tells us the maximum strand number in a list which is of the type dom*)
 primrec dom_Max_list::"endpt list \<Rightarrow> nat"
 where
 "dom_Max_list [] = 0"|
-"dom_Max_list (Cons x xs) = (case (type x) of 
-                                             domain \<Rightarrow> (max (snd x) (dom_Max_list xs))
+"dom_Max_list (Cons x xs) = (case (type x) of
+                                             domain \<Rightarrow> (max (str_number x) (dom_Max_list xs))
                                              |codomain\<Rightarrow> (dom_Max_list xs))"
-(*we define functions related to shifting of the codomain and domain*)
+(*the following function gives us n such that n is the maximum number assigned to an end of
+a type codomain*)
+primrec codom_Max_list::"endpt list \<Rightarrow> nat"
+where
+"codom_Max_list [] = 0"|
+"codom_Max_list (Cons x xs) = (case (type x) of
+                                               codomain \<Rightarrow> (max (str_number x) (codom_Max_list xs))
+                                              |domain\<Rightarrow> (codom_Max_list xs))"
+
+
+(*we define functions related to shifting of the codomain and domain by 1/2 strands to left*)
 definition codom_left_shift::"(endpt \<Rightarrow> bool) \<Rightarrow> endpt \<Rightarrow> endpt"
 where
-"codom_left_shift P x \<equiv>(if ((type x = codomain)\<and> (P x)) then (codom (snd x +1)) else x)"
+"codom_left_shift P x \<equiv>(if ((type x = codomain)\<and> (P x)) then (codom (str_number x +1)) else x)"
 
 
 abbreviation codom_double_left_shift::"(endpt\<Rightarrow> bool) \<Rightarrow> endpt \<Rightarrow> endpt"
 where
-"codom_double_left_shift P x \<equiv>(if ((type x = codomain)\<and> (P x)) then (codom (snd x +2)) else x)"
+"codom_double_left_shift P x \<equiv>(if ((type x = codomain)\<and> (P x)) then (codom (str_number x +2)) else x)"
 
 definition codom_double_left_shift_tuple::"(endpt\<Rightarrow>bool)\<Rightarrow> (endpt \<times> endpt) \<Rightarrow> (endpt \<times> endpt)"
 where
 "codom_double_left_shift_tuple P x \<equiv>
     (codom_double_left_shift P (fst x),codom_double_left_shift P (snd x))  "
-   
-
+  
 primrec split_codom_double_left_shift::"(endpt \<Rightarrow> bool) \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> (endpt \<times> endpt) list"
  where
 "split_codom_double_left_shift P [] = []"
-|"split_codom_double_left_shift P (x#xs) = (codom_double_left_shift_tuple P x)#xs  "
+|"split_codom_double_left_shift P (x#xs) = (codom_double_left_shift_tuple P x)#(split_codom_double_left_shift P xs)"
 
-
-(*right shift*)
+(*we define functions equivalent to above for right shift*)
 definition codom_right_shift::"(endpt \<Rightarrow> bool) \<Rightarrow> endpt \<Rightarrow> endpt"
 where
-"codom_right_shift P x \<equiv>(if ((type x = codomain)\<and> (P x)) then (codom (snd x- 1)) else x)"
-
+"codom_right_shift P x \<equiv>(if ((type x = codomain)\<and> (P x)) then (codom (str_number x- 1)) else x)"
 
 abbreviation codom_double_right_shift::"(endpt\<Rightarrow> bool) \<Rightarrow> endpt \<Rightarrow> endpt"
 where
-"codom_double_right_shift P x \<equiv>(if ((type x = codomain)\<and> (P x)) then (codom (snd x- 2)) else x)"
+"codom_double_right_shift P x \<equiv>(if ((type x = codomain)\<and> (P x)) then (codom (str_number x- 2)) else x)"
 
 definition codom_double_right_shift_tuple::"(endpt\<Rightarrow>bool)\<Rightarrow> (endpt \<times> endpt) \<Rightarrow> (endpt \<times> endpt)"
 where
 "codom_double_right_shift_tuple P x \<equiv>
-    (codom_double_right_shift P (fst x),codom_double_right_shift P (snd x))  "
-   
+    (codom_double_right_shift P (fst x),codom_double_right_shift P (snd x))"  
 
 primrec split_codom_double_right_shift::"(endpt \<Rightarrow> bool) \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> (endpt \<times> endpt) list"
  where
 "split_codom_double_right_shift P [] = []"
-|"split_codom_double_right_shift P (x#xs) = (codom_double_right_shift_tuple P x)#xs  "
-(*the following function gives us n such that n is the maximum number assigned to an end of 
-a type codomain*)
-primrec codom_Max_list::"endpt list \<Rightarrow> nat"
-where
-"codom_Max_list [] = 0"|
-"codom_Max_list (Cons x xs) = (case (type x) of 
-                                               codomain \<Rightarrow> (max (snd x) (codom_Max_list xs))
-                                              |domain\<Rightarrow> (codom_Max_list xs))"
+|"split_codom_double_right_shift P (x#xs) = (codom_double_right_shift_tuple P x)#
+(split_codom_double_right_shift P xs)  "
+
+value
+"codom_double_right_shift_tuple  (\<lambda>x.(str_number x>2)) (codom 5, codom 6)"
 
 (*If one of the two elements of a tuple is i then it replaces is with j*)
 definition swap_with::"endpt \<Rightarrow> endpt \<Rightarrow> endpt \<Rightarrow> endpt"
@@ -120,7 +114,7 @@ where
 endpts of the former*)
 primrec linearize::"(endpt \<times> endpt) list \<Rightarrow> endpt list"
 where
-"linearize [] = []" 
+"linearize [] = []"
 |"linearize (x#xs) = (fst x)#(snd x)#(linearize xs)"
 
 (*filters out all the elements with endtype domain*)
@@ -133,14 +127,13 @@ definition codomain_filter::"endpt list \<Rightarrow> endpt list"
 where
 "codomain_filter xs \<equiv> [x<- xs. ((type x)=codomain)]"
 
-
 (* The following function defines vertical action*)
 definition vert_act::"nat \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> (endpt \<times> endpt) list"
 where
-"vert_act n xs \<equiv> 
- (if (find (codom n) xs) 
-    then xs 
-    else  (codom (codom_Max_list (linearize xs) +1 ), dom (dom_Max_list (linearize xs) + 1))#xs)"
+"vert_act n xs \<equiv>
+ (if (find (codom n) xs)
+    then xs
+    else  (dom (dom_Max_list (linearize xs) + 1), codom (codom_Max_list (linearize xs) +1))#xs)"
  
 definition swap_act::"nat \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> (endpt \<times> endpt) list"
 where
@@ -149,14 +142,14 @@ where
       then (swap_in (codom n) (codom (n+1)) xs)
            else if (find (codom n) xs)
               then ((dom ((dom_Max_list (linearize xs))+1), codom n)#swap_in (codom n) (codom (n+1)) xs)
-                  else 
+                  else
 [(dom (dom_Max_list (linearize xs)+2),codom n),((dom (dom_Max_list (linearize xs)+1),codom (n+1)))]
-                          @xs)  " 
-   
-definition cup_act::"nat \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> (endpt \<times> endpt) list"
+                          @xs)"
+  
+definition cap_act::"nat \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> (endpt \<times> endpt) list"
 where
-"cup_act n xs \<equiv> 
-(codom n, codom (n+1))#(split_codom_double_left_shift (\<lambda>x.(type x=codomain)\<and>(snd x>n)) xs)"
+"cap_act n xs \<equiv>
+(codom n, codom (n+1))#(split_codom_double_left_shift (\<lambda>x.(type x=codomain)\<and>(str_number x \<ge> n)) xs)"
 
 definition belongs_to::"'a \<Rightarrow> 'a list \<Rightarrow> bool"
 where
@@ -167,75 +160,100 @@ where
 "delete_from_list a [] = []"|
 "delete_from_list a (x#xs) = (if (x=a) then (delete_from_list a xs) else (x#(delete_from_list a xs)))"
 
-
-(*it adds nat option to nat*)
-primrec nat_option_add::"nat\<Rightarrow>nat option\<Rightarrow> nat option" (infixl "+" 67)
-where
-"nat_option_add n None = None"
-|"nat_option_add n (Some m) = (Some (m+n))"
-
-(*it gives us Some n if a accurs in the list at the nth position*)
-primrec first_occurance::"endpt \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> nat option"
-where
-"first_occurance a [] = None"
-|"first_occurance a (x#xs) = (if (((fst x)= a)\<or>((snd x)=a)) then Some 0 else  (1+first_occurance a xs))"
- 
-(*if a is one of the two elements, then the following gives the other coefficient of the tuple else it gives
-the same element*) 
-definition other_end::" endpt \<Rightarrow>  (endpt \<times> endpt) \<Rightarrow> endpt"
+(*if a is one of the two elements, then the following gives the other element else it gives
+the first element*)
+definition other_end::"endpt \<Rightarrow>  (endpt \<times> endpt) \<Rightarrow> endpt"
 where
 "other_end a x \<equiv> (if (fst x)= a then (snd x) else if (snd  x = a) then (fst x) else a)"
 
-
-(*if a is one of the two elements, then the following gives other coefficient of the tuple in the first
-occurance  else it returns the same element*) 
-primrec other_end_list::"endpt \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> endpt"
+primrec prelim_other_end_list::"endpt \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> endpt"
 where
-"other_end_list a [] = a"
-|"other_end_list a (x#xs) = 
-           (if ((other_end_list a xs) \<noteq> a) then (other_end_list a xs) else (other_end a x))" 
+"prelim_other_end_list a [] = a"
+|"prelim_other_end_list a (x#xs) =
+           (if ((prelim_other_end_list a xs) \<noteq> a) then (prelim_other_end_list a xs) else a)"
 
-(*It deletes every tuple in the list which contains the element as one of the constituent elements of
-the tuple*)
+definition other_end_list ::"endpt \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> endpt"
+where
+"other_end_list a xs \<equiv> (if (prelim_other_end_list a xs = a)
+                         then (dom (dom_Max_list (linearize xs) + 1))
+                                   else (prelim_other_end_list a xs))" 
+
+definition readjust::"endpt \<times> endpt \<Rightarrow> endpt \<times> endpt"
+where
+"readjust x \<equiv> (if ((fst x)=(snd x)) then (fst x,(dom (str_number (fst x) + 1))) else x)"
+
 primrec delete_containing::"endpt \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> (endpt \<times> endpt) list"
 where
 "delete_containing a [] = []"
-|"delete_containing a (x#xs) = (if ((fst x = a)\<or>(snd x = a)) 
+|"delete_containing a (x#xs) = (if ((fst x = a)\<or>(snd x = a))
                                 then (delete_containing a xs)
-                                        else (x#(delete_containing a xs)))" 
+                                        else (x#(delete_containing a xs)))"
 
-definition cap_check::"nat \<Rightarrow> (endpt \<times> endpt) list  \<Rightarrow> (endpt \<times> endpt) list"
+definition cup_check::"nat \<Rightarrow> (endpt \<times> endpt) list  \<Rightarrow> (endpt \<times> endpt) list"
 where
-"cap_check n xs = (other_end_list (codom n) xs, other_end_list (codom (n+1)) xs)
-                     #( split_codom_double_right_shift  (\<lambda>x.(type x=codomain)\<and>(snd x>(n+1))) (delete_containing (codom (n+1)) (delete_containing (codom n) xs)))"
+"cup_check n xs =
+split_codom_double_right_shift (\<lambda>x.(type x=codomain)\<and>(str_number x >(n+1)))
+ (readjust (other_end_list (codom n) xs, other_end_list (codom (n+1)) xs)
+#(delete_containing (codom (n+1)) (delete_containing (codom n) xs)))"
 
 (*return a blank list if the some condition is not satisfied and finish the function*)
-definition cap_act::"nat \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> (endpt \<times> endpt) list"
+definition cup_act::"nat \<Rightarrow> (endpt \<times> endpt) list \<Rightarrow> (endpt \<times> endpt) list"
 where
-"cap_act n xs \<equiv>
- (if  (belongs_to (codom n, codom (n+1)) xs)
-  then  split_codom_double_right_shift  (\<lambda>x.(type x=codomain)\<and>(snd x>(n+1))) 
-                         (delete_from_list (codom n, codom (n+1)) xs) 
-   else cap_check n xs)"
+"cup_act n xs \<equiv>
+ (if 
+    (belongs_to (codom n, codom (n+1)) xs)
+  then
+    split_codom_double_right_shift
+      (\<lambda>x.(type x=codomain)\<and>(str_number x > (n+1))) (delete_from_list  (codom n, codom (n+1)) xs)
+   else cup_check n xs)"
 
 primrec block_action::"block \<Rightarrow> (endpt \<times> endpt)  list \<times> nat  \<Rightarrow>  (endpt \<times> endpt) list \<times> nat"
 where
-"block_action [] ls = ([],snd ls)"
-|"block_action (x#xs) ls = 
+"block_action [] ls = ls"
+|"block_action (x#xs) ls =
    (case x of
-     vert \<Rightarrow> ((vert_act (nat (codomain_block xs)) (fst (block_action xs  ls))),(snd ls))
-    |over \<Rightarrow> ((swap_act (nat (codomain_block xs))  (fst (block_action xs  ls)),(snd ls)))
-|under \<Rightarrow> ((swap_act (nat (codomain_block xs))  (fst (block_action xs ls))),(snd ls))
-|cup \<Rightarrow>    ((cup_act (nat (codomain_block xs))  (fst (block_action xs  ls))),(snd ls))
-|cap \<Rightarrow>  ((cap_act (nat (codomain_block xs))  (fst (block_action xs  ls))),
- (if  (belongs_to (codom (nat (codomain_block xs)), codom ((nat (codomain_block xs))+1)) (fst ls))
-  then ((snd ls)+1) else (snd ls))))"
-   
+     vert \<Rightarrow> ((vert_act (nat (domain_block xs)+1) (fst (block_action xs  ls))),(snd ls))
+    |over \<Rightarrow> ((swap_act (nat (domain_block xs)+1)  (fst (block_action xs  ls)),(snd ls)))
+|under \<Rightarrow> ((swap_act (nat (domain_block xs)+1)  (fst (block_action xs ls))),(snd ls))
+|cap \<Rightarrow>    ((cap_act (nat (domain_block xs)+1)  (fst (block_action xs  ls))),(snd ls))
+|cup \<Rightarrow>  ((cup_act (nat (domain_block xs)+1)  (fst (block_action xs  ls))),
+ (if  (belongs_to (codom (nat (domain_block xs)+1), codom ((nat (domain_block xs))+2))  (fst (block_action xs  ls)))
+  then ((snd (block_action xs  ls))+1) else (snd (block_action xs  ls)))))"
+  
 primrec wall_action::"wall \<Rightarrow> (endpt \<times> endpt)  list \<times> nat  \<Rightarrow>  (endpt \<times> endpt) list \<times> nat"
 where
 "wall_action (basic x) ls = block_action x ls"
 |"wall_action (w*ws) ls = block_action w (wall_action ws ls)"
 
 definition component_number::"wall \<Rightarrow> nat"
-where 
+where
 "(component_number w) \<equiv> snd (wall_action w ([],0))"
+
+
+(*problem lies with split_codomain_right*)
+value "cup_check 1 []"
+value "block_action [cup,cap,cup] ([],0)"
+value "wall_action ([cap,cap]*(basic [cap,cap])) ([],0)"
+(*hopf link has two components*)
+value "wall_action ([cup,cup]*[vert,over,vert]*[vert,over,vert]*(basic [cap,cap])) ([],0)"
+(*unknot diagram with a cross1ing has a single component*)
+value "wall_action ([cup,cup]*[vert,over,vert]*(basic [cap,cap])) ([],0)"
+
+value "block_action [cup] ([(codom 5, codom 6), (codom 3, codom 4), (codom 1, codom 2)], 0)"
+value "block_action [cup] ([(codom 3, codom 4), (codom 1, codom 2)],4)"
+value " (belongs_to (codom (nat (domain_block [cup])+1), codom ((nat (domain_block [cup]))+2)) (fst ([(codom 1, codom 2)], 1)))"
+
+(*"([(endpt.dom 5, codom 7), (endpt.dom 4, codom 5), (endpt.dom 3, codom 6), (endpt.dom 2, codom 3),
+   (endpt.dom 1, codom 4), (codom 1, codom 2)],*)
+value "(codom (nat (domain_block [cup])+1), codom ((nat (domain_block [cup]))+2))"
+value "block_action ([vert,cup]) ([],0)"
+value "block_action [cup] ([(codom 1, codom 2)], 1)"
+value "block_action [vert] ([(codom 1, codom 2)], 0)"
+value "block_action [cup] ([(codom 3, codom 4), (codom 1, codom 2)], 0)"
+value "(find_both  (codom 1) (codom 2) [(endpt.dom 2, codom 1), (endpt.dom 1, codom 2)])"
+value "(swap_act 1 (swap_act 1 []))"
+value "swap_act 1 ([(endpt.dom 2, codom 1), (endpt.dom 1, codom 2)])"
+value "cup_act 0 [(codom 2, codom 3), (codom 0, codom 1)]"
+value "block_action  [cup] ([(codom 2, codom 3), (codom 0, codom 1)], 0)"
+
+value "(delete_from_list (codom 1, codom 0) [(codom 2, codom 3), (codom 0, codom 1)])" 
